f82ba2653415875e0eccf9bf30612d2fa176339c
lin-br <wes.rsilva@gmail.com>
2025-06-21 16:48:58 -0300

    refactor: extract statement to a function

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -11,6 +11,12 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s, want %s", got, want)
 		}
 	}
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
 
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
@@ -29,10 +35,7 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
+		assertError(t, err)
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
 	})
 }
