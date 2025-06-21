0a0fbff50b4897d721915a2c0e6f988fd7ec47c7
lin-br <wes.rsilva@gmail.com>
2025-06-21 16:43:54 -0300

    test: create a test to insufficient funds

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -24,4 +24,15 @@ func TestWallet(t *testing.T) {
 
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	})
 }
