a061bd2186896dd82f0fb5b3874b166d170feef1
lin-br <wes.rsilva@gmail.com>
2025-06-21 17:15:31 -0300

    refactor: add error handler to green test

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -14,8 +14,9 @@ func TestWallet(t *testing.T) {
 	})
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
@@ -36,6 +37,7 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Errorf("got %s, want %s", got, want)
 	}
 }
+
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
 	if got == nil {
@@ -43,5 +45,12 @@ func assertError(t testing.TB, got, want error) {
 	}
 	if !errors.Is(got, want) {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
