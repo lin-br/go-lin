bd5520ab16c2c997df2a86e4a0e121d861fb9d7a
lin-br <wes.rsilva@gmail.com>
2025-06-20 21:00:09 -0300

    test: initial the SumAllTails tests

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+func assertCorrectMessage(t testing.TB, got, want []int) {
+	t.Helper()
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
@@ -32,7 +39,12 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v and want %v", got, want)
-	}
+	assertCorrectMessage(t, got, want)
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	assertCorrectMessage(t, got, want)
 }
