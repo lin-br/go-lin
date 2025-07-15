21e3d10b6a85e2dc32531a143b6b4d29a42049f9
lin-br <wes.rsilva@gmail.com>
2025-07-15 17:30:01 -0300

    feat: add the select statement into Racer function

--- learn-go/11-select/racer.go
+++ learn-go/11-select/racer.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(url1, url2 string) (winner string) {
-	duration1 := measureResponseTime(url1)
-	duration2 := measureResponseTime(url2)
-
-	if duration1 < duration2 {
+	select {
+	case <-ping(url1):
 		return url1
+	case <-ping(url2):
+		return url2
 	}
-	return url2
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	channels := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(channels)
+	}()
+	return channels
 }
