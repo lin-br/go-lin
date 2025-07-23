74bba733855b264baa3952844e87540355c1678c
lin-br <wes.rsilva@gmail.com>
2025-07-23 09:59:46 -0300

    refactor: refactor the wal test

--- learn-go/12-reflection/walk_test.go
+++ learn-go/12-reflection/walk_test.go
@@ -1,22 +1,36 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	t.Run("struct with one string field", func(t *testing.T) {
-		expected := "Chris"
-		var got []string
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name: "struct with one string field",
+			Input: struct {
+				Name string
+			}{"Chris"},
+			ExpectedCalls: []string{"Chris"},
+		},
+	}
 
-		x := struct {
-			Name string
-		}{expected}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
 
-		walk(x, func(input string) {
-			got = append(got, input)
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
 		})
-
-		if len(got) != 1 {
-			t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
-		}
-	})
+	}
 }
