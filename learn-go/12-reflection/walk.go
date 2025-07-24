7dc331c97aa24e8a291f6f0ad9a79063f54c545b
lin-br <wes.rsilva@gmail.com>
2025-07-24 20:46:17 -0300

    feat: add the channels in the reflection class

--- learn-go/12-reflection/walk.go
+++ learn-go/12-reflection/walk.go
@@ -20,6 +20,10 @@ func walk(x interface{}, fn func(input string)) {
 		for _, key := range val.MapKeys() {
 			walk(val.MapIndex(key).Interface(), fn)
 		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
+		}
 	default:
 		// Do nothing for non-string fields
 	}
