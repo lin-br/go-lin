1df5a2a3513c9f3463a428ae79c6f9722a5cd094
lin-br <wes.rsilva@gmail.com>
2025-07-25 17:00:03 -0300

    refactor: add the context as a parameter to the Fetch method

--- learn-go/14-context/context.go
+++ learn-go/14-context/context.go
@@ -1,27 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(context context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		context := r.Context()
-		data := make(chan string, 1)
-		go func() {
-			data <- store.Fetch()
-		}()
-		select {
-		case fetched := <-data:
-			_, _ = fmt.Fprint(w, fetched)
-		case <-context.Done():
-			store.Cancel()
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
 		}
+
+		fmt.Fprint(w, data)
 	}
 }
