3954697d3677da27fbf75b6ef364be349c171ab4
lin-br <wes.rsilva@gmail.com>
2025-07-25 16:33:17 -0300

    feat: increase the Server with context.Done()

--- learn-go/14-context/context.go
+++ learn-go/14-context/context.go
@@ -12,7 +12,16 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+		context := r.Context()
+		data := make(chan string, 1)
+		go func() {
+			data <- store.Fetch()
+		}()
+		select {
+		case fetched := <-data:
+			_, _ = fmt.Fprint(w, fetched)
+		case <-context.Done():
+			store.Cancel()
+		}
 	}
 }
