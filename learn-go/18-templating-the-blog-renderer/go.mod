07ba14bcdafdb2779b218f5b7f2c96e74fe1feae
lin-br <wes.rsilva@gmail.com>
2025-08-24 17:51:25 -0300

    feat: initialize the blog renderer with approvals library

--- learn-go/18-templating-the-blog-renderer/go.mod
+++ learn-go/18-templating-the-blog-renderer/go.mod
@@ -1,3 +1,5 @@
 module github.com/lin-br/go-lin/blogrenderer
 
 go 1.24
+
+require github.com/approvals/go-approval-tests v1.6.0 // indirect
---
+++ learn-go/18-templating-the-blog-renderer/renderer.go
@@ -0,0 +1,25 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+func Render(w io.Writer, p Post) error {
+	templateParsed, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return err
+	}
+
+	if err := templateParsed.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
