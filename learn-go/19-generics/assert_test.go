d49acda86ad4fbb20d546591626d32e286e9c805
lin-br <wes.rsilva@gmail.com>
2025-08-26 09:14:30 -0300

    test: introduce the generics tests to slices

--- learn-go/19-generics/assert_test.go
+++ learn-go/19-generics/assert_test.go
@@ -1,10 +1,20 @@
 package generics_test
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func AssertSlicesEqual[T comparable](t *testing.T, got, want []T) {
+	t.Helper()
+	if !slices.Equal(got, want) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
---
+++ learn-go/19-generics/sum_test.go
@@ -0,0 +1,52 @@
+package generics_test
+
+import (
+	"testing"
+
+	"github.com/lin-br/go-lin/generics"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := generics.Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d and want %d, given %v", got, want, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := generics.Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d and want %d, given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := generics.SumAll([]int{1, 2, 0, 9})
+	want := 12
+
+	AssertEqual(t, got, want)
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := generics.SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		AssertSlicesEqual(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := generics.SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		AssertSlicesEqual(t, got, want)
+	})
+}
