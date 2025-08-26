9508a3b95d0292a054f26b580cf7d9b31a619436
lin-br <wes.rsilva@gmail.com>
2025-08-26 20:32:46 -0300

    feat: extend the generic.Transaction

--- learn-go/19-generics/bank_test.go
+++ learn-go/19-generics/bank_test.go
@@ -7,20 +7,22 @@ import (
 )
 
 func TestBadBank(t *testing.T) {
-	transactions := []generics.Transaction{
-		{
-			From: "Chris",
-			To:   "Riya",
-			Sum:  100,
-		},
-		{
-			From: "Adil",
-			To:   "Chris",
-			Sum:  25,
-		},
+	var (
+		riya  = generics.Account{Name: "Riya", Balance: 100}
+		chris = generics.Account{Name: "Chris", Balance: 75}
+		adil  = generics.Account{Name: "Adil", Balance: 200}
+
+		transactions = []generics.Transaction{
+			generics.NewTransaction(chris, riya, 100),
+			generics.NewTransaction(adil, chris, 25),
+		}
+	)
+
+	newBalanceFor := func(account generics.Account) float64 {
+		return generics.NewBalanceFor(account, transactions).Balance
 	}
 
-	AssertEqual(t, generics.BalanceFor(transactions, "Riya"), 100)
-	AssertEqual(t, generics.BalanceFor(transactions, "Chris"), -75)
-	AssertEqual(t, generics.BalanceFor(transactions, "Adil"), -25)
+	AssertEqual(t, newBalanceFor(riya), 200)
+	AssertEqual(t, newBalanceFor(chris), 0)
+	AssertEqual(t, newBalanceFor(adil), 175)
 }
