0bf5c49f633aa61ae4153b1ec13a59e4a68ee1d0
lin-br <wes.rsilva@gmail.com>
2025-08-28 07:44:54 -0300

    feat: add a new route into http server

--- applications/simple-http-server/api_test.go
+++ applications/simple-http-server/api_test.go
@@ -106,3 +106,17 @@ func TestStoreWins(t *testing.T) {
 		}
 	})
 }
+
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &api.PlayerServer{Store: &store}
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
